dc48e73d3428d5dd3f431c0165212d42b1fd893a
mr-mph <88854453+mr-mph@users.noreply.github.com>
2024-06-25 20:55:47 -0700

    qol improvements including the ability to select the visualizer and a new system for going through presets in order

--- src/visualizer/visualizer.js
+++ src/visualizer/visualizer.js
@@ -1,16 +1,16 @@
 var readyStateCheckInterval = setInterval(() => {
-	if (document.readyState === "complete") {
+  if (document.readyState === "complete") {
     clearInterval(readyStateCheckInterval);
 
-    const noAudioOverlay = document.getElementById('noAudioOverlay');
-    const canvas = document.getElementById('canvas');
-    const visualizer = butterchurn.default.createVisualizer(null, canvas , {
+    const noAudioOverlay = document.getElementById("noAudioOverlay");
+    const canvas = document.getElementById("canvas");
+    const visualizer = butterchurn.default.createVisualizer(null, canvas, {
       width: 800,
       height: 600,
       mesh_width: 64,
       mesh_height: 48,
       pixelRatio: window.devicePixelRatio || 1,
-      textureRatio: 1
+      textureRatio: 1,
     });
     visualizer.loadExtraImages(imageDataButterchurnPresets.default);
 
@@ -18,7 +18,11 @@ var readyStateCheckInterval = setInterval(() => {
     const presetKeys = Object.keys(presets);
     const presetIndexHist = [];
     let presetCycle = true;
-    let nextPreset, prevPreset, restartCycleInterval, cycleInterval, toggleRandomize;
+    let nextPreset,
+      prevPreset,
+      restartCycleInterval,
+      cycleInterval,
+      toggleRandomize;
 
     const setVisualizerSize = () => {
       const vizWidth = window.innerWidth;
@@ -32,9 +36,25 @@ var readyStateCheckInterval = setInterval(() => {
       noAudioOverlay.style.height = `${vizHeight}px`;
     };
 
+    const promptForMode = () => {
+      // set visualizer to what the user wants
+      const visName = prompt("What visualizer?");
+
+      try {
+        const presetIdx = presets[visName];
+        presetIndexHist.push(visName);
+        visualizer.loadPreset(presetIdx, 0);
+      } catch (e) {
+        alert("Invalid mode");
+      }
+    };
+
     nextPreset = (blendTime) => {
-      const presetIdx = presets[presetKeys[Math.floor(presetKeys.length * Math.random())]];
-      presetIndexHist.push(presetIdx);
+      const index = presetKeys.indexOf(presetIndexHist.at(-1));
+      const visName = presetKeys[index + 1];
+      const presetIdx = presets[visName];
+      console.log(visName);
+      presetIndexHist.push(visName);
       visualizer.loadPreset(presetIdx, blendTime);
       restartCycleInterval();
     };
@@ -45,12 +65,14 @@ var readyStateCheckInterval = setInterval(() => {
         presetIndexHist.pop();
 
         if (presetIndexHist.length > 0) {
-          presetIdx = presetIndexHist[presetIndexHist.length - 1];
+          presetIdx = presets[presetIndexHist.at(-1)];
         } else {
-          presetIdx = presets[presetKeys[Math.floor(presetKeys.length * Math.random())]];
+          presetIdx =
+            presets[presetKeys[Math.floor(presetKeys.length * Math.random())]];
         }
       } else {
-        presetIdx = presets[presetKeys[Math.floor(presetKeys.length * Math.random())]];
+        presetIdx =
+          presets[presetKeys[Math.floor(presetKeys.length * Math.random())]];
       }
 
       visualizer.loadPreset(presetIdx, blendTime);
@@ -58,16 +80,15 @@ var readyStateCheckInterval = setInterval(() => {
     };
 
     restartCycleInterval = () => {
-      if (cycleInterval) {
-        clearInterval(cycleInterval);
-        cycleInterval = null;
-      }
-
-      if (presetCycle) {
-        cycleInterval = setInterval(() => {
-          nextPreset(2.7);
-        }, 24000);
-      }
+      // if (cycleInterval) {
+      //   clearInterval(cycleInterval);
+      //   cycleInterval = null;
+      // }
+      // if (presetCycle) {
+      //   cycleInterval = setInterval(() => {
+      //     nextPreset(2.7);
+      //   }, 24000);
+      // }
     };
 
     toggleRandomize = () => {
@@ -76,34 +97,43 @@ var readyStateCheckInterval = setInterval(() => {
     };
 
     setVisualizerSize();
-    nextPreset(0);
+    const presetIdx = presets["$$$ Royal - Mashup (197)"];
+    presetIndexHist.push("$$$ Royal - Mashup (197)");
+    visualizer.loadPreset(presetIdx, 0);
 
     chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-      if (request.type === 'startRendering') {
-        noAudioOverlay.style.display = 'none';
-      } else if (request.type === 'stopRendering') {
-        noAudioOverlay.style.display = 'flex';
-      } else if (request.type === 'audioData') {
+      if (request.type === "startRendering") {
+        noAudioOverlay.style.display = "none";
+      } else if (request.type === "stopRendering") {
+        noAudioOverlay.style.display = "flex";
+      } else if (request.type === "audioData") {
         visualizer.render(request.data);
       }
 
       sendResponse();
     });
 
-    document.addEventListener('keydown', (e) => {
-      if (e.which === 32 || e.which === 39) { // SPACE or ->
+    document.addEventListener("keydown", (e) => {
+      if (e.which === 32 || e.which === 39) {
+        // SPACE or ->
         nextPreset(5.7);
-      } else if (e.which === 8 || e.which === 37) { // BACKSPACE or <-
-        prevPreset(0);
-      } else if (e.which === 72) { // H
+      } else if (e.which === 8 || e.which === 37) {
+        // BACKSPACE or <-
+        prevPreset(5.7);
+      } else if (e.which === 72) {
+        // H
         nextPreset(0);
-      } else if (e.which === 82) { // R
-        toggleRandomize();
+        // } else if (e.which === 82) {
+        //   // R
+        //   toggleRandomize();
+      } else if (e.which === 81) {
+        // Q
+        promptForMode();
       }
     });
 
-    window.addEventListener('resize', () => {
+    window.addEventListener("resize", () => {
       setVisualizerSize();
     });
-	}
+  }
 }, 10);
